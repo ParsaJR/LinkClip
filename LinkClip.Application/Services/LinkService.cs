15d3e3aabad09e30e2ced3bb8a9ed04307c2483a
parsa.javadian <parsa.javadian@yahoo.com>
2023-09-17 17:55:31 +0330

    linkService comment

--- LinkClip.Application/Services/LinkService.cs
+++ LinkClip.Application/Services/LinkService.cs
@@ -101,7 +101,7 @@ namespace LinkClip.Application.Services
             shortUrl.OriginalUrl = uri;
             shortUrl.CreateDate = DateTime.Now;
             shortUrl.Token = Generate.Token();
-            shortUrl.Value = new Uri($"https://parsajr.xyz/{shortUrl.Token}"); 
+            shortUrl.Value = new Uri($"https://parsajr.xyz/{shortUrl.Token}"); //Create Link With Your BaseURL
             return shortUrl;
         }
     }
